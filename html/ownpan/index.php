5fcc68590941fb55ea3d78372e02fbb265564856
PathsCode <pathscode@gmail.com>
2022-11-13 20:47:50 +0100

    Rules: Appearance

--- html/ownpan/index.php
+++ html/ownpan/index.php
@@ -32,8 +32,8 @@ if (file_exists("/srv/data/sysname")) {
                 <input autocomplete="off" name="password" type="password" class="none">
                 <div class="flex">
                     <div class="flex">
-                        <label>UserName </label>
-                        <input id="name" name="username" placeholder="UserName" type="text" autocomplete="off">
+                        <label>Username </label>
+                        <input id="name" name="username" placeholder="Username" type="text" autocomplete="off">
                     </div>
                     <div class="flex">
                         <label>Password </label>
@@ -45,13 +45,40 @@ if (file_exists("/srv/data/sysname")) {
             </form>
         </div>
         <h1 class="rules-title">Regole</h1>
-        <form class="reles">
+        <form action="php_/rules.php" method="post" class="reles">
             <?php for ($releId = 1; $releId <= 8; $releId ++) : ?>
                 <div class="rele">
-                    <h2>Relè <?= $releId; ?></h2>
+                    <div class="rele-title flex">
+                        <h2>RELÈ <?= $releId; ?></h2>
+                        <img src="/css_/x-png-33.png" class="delete-rule cursor"/>
+                    </div>
+                    <div class="rule-main rule-info">
+                        <span>SE</span>
+                        <input placeholder="Sensore" class="sensor-input">
+                        <select class="select-comparison">
+                            <option><</option>
+                            <option>=</option>
+                            <option>></option>
+                        </select>
+                        <input placeholder="Valore" class="sensor-value">
+                        <span class="unit"></span>
+                    </div>
+                    <div class="rule-delay rule-info">
+                        <span>ACCENDI RELÈ PER</span>
+                        <input placeholder="0" value="0" class="duration-value">
+                        <span>SECONDI E DISATTIVALO PER</span>
+                        <input placeholder="0" value="0" class="delay-value">
+                        <span>SECONDI</span>
+                    </div>
+                    <div class="rule-time rule-info">
+                        <span>La Regola è in funzione dalle</span>
+                        <input placeholder="HH:MM" value="00:00" class="start-time time-selection">
+                        <span>alle</span>
+                        <input placeholder="HH:MM" value="23:59" class="end-time time-selection">
+                    </div>
                 </div>
             <?php endfor; ?>
-            <input name="submit" type="submit" value="Salva modifiche" class="confirm-button update-rules">
+            <input type="submit" name="Salva" value="Salva Modifiche" class="confirm-button update-rules cursor">
         </form>
     </body>
 
---
+++ html/ownpan/php_/rules.php
@@ -0,0 +1,8 @@
+<?php
+
+    // Basic Rules Stuff
+
+    // Edit Rules (Add / Edit / Delete)
+    if (isset($_POST['Salva'])) {
+
+    }
